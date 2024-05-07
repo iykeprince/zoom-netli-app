c0794257f9b861bf440a9bfedbf35814938f44a6
Iyke Israel <iykeprince48@gmail.com>
2024-05-07 21:30:59 +0100

    clicup

--- app/auth/route.ts
+++ app/auth/route.ts
@@ -2,21 +2,21 @@ import { getDeeplink, getToken, rand } from "@/libs/app_request";
 import {NextRequest, NextResponse} from "next/server";
 
 export async function GET (request: NextRequest) {
-    // const greeting = "Hello World!!"
-    // const json = {
-    //     greeting
-    // };
-    const state = rand('base64');
-    const verifier = rand('ascii');
+    const greeting = "Hello World!!"
+    const json = {
+        greeting
+    };
+//     const state = rand('base64');
+//     const verifier = rand('ascii');
 
-    // return NextResponse.json(json);
-    const searchParams = request.nextUrl.searchParams;
-    const code = searchParams.get('code') as  string;
-    console.log(`Code: ${code}`);
-    const { access_token: accessToken } = await getToken(code, verifier)
-    console.log(`Access token: ${accessToken}`);
-    const deeplink = await getDeeplink(accessToken)
-    console.log(`Deeplink: ${deeplink}`);
-   return NextResponse.redirect(deeplink)
-return NextResponse.json({accessToken})
+    return NextResponse.json(json);
+//     const searchParams = request.nextUrl.searchParams;
+//     const code = searchParams.get('code') as  string;
+//     console.log(`Code: ${code}`);
+//     const { access_token: accessToken } = await getToken(code, verifier)
+//     console.log(`Access token: ${accessToken}`);
+//     const deeplink = await getDeeplink(accessToken)
+//     console.log(`Deeplink: ${deeplink}`);
+//    return NextResponse.redirect(deeplink)
+// return NextResponse.json({accessToken})
 }
